f5315ad0e01eada7fda2803ba527fcc7608565c0
auser73 <auser7312@gmail.com>
2025-04-02 15:08:44 +0900

    Update blog_crawler.py

--- blog_crawler.py
+++ blog_crawler.py
@@ -35,5 +35,8 @@ def crawl():
     
     return jsonify({"status": "fail", "message": "No valid content found."})
 
+import os
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
